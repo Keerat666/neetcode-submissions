3450e34fd3395a569da7024f3c1b6137eb9e9f30
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2026-08-06 23:54:42 -0600

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int [] freq = new int[nums.length+1];
+
+        for(int i=0;i<nums.length;i++)
+        {
+            freq[nums[i]]=1;
+        }
+
+        for(int i=0;i<=nums.length;i++)
+        {
+            if(freq[i]!=1)
+                return i;
+
+        }
+
+        return -1;
+        
+    }
+}
