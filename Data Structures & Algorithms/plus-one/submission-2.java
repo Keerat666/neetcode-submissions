6105a946944c6c372dacdeac4d030d054317aa38
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2026-08-07 22:37:14 -0600

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int nineCounter=0;
+
+
+        for(int i=digits.length-1;i>=0;i--)
+        {                
+                if(digits[i]<9)
+                {
+                    digits[i]=digits[i]+1;
+                    return digits;
+                }
+                  
+                else if(digits[i]==9)
+                {
+                    digits[i]=0;
+                    nineCounter++;
+                }
+        }
+
+        if(nineCounter==digits.length)
+            {
+                int ans[] = new int[digits.length+1];
+                ans[0]=1;
+                for(int i=1;i<ans.length;i++)
+                    ans[i]=0;
+                return ans;
+            }
+        return digits;
+
+    }
+}
+
+
