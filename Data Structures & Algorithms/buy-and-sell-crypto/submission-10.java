0b3a0acb28558d15faf6ad4fb533487ac28fb209
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2026-08-25 01:46:43 -0600

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int start = 0;
+        int end = 1;
+        int ans = 0;
+
+        while (end < prices.length) {
+            int profit = prices[end] - prices[start];
+            ans = Math.max(ans, profit);
+
+            if (prices[end] < prices[start]) {
+                start = end;
+            }
+            end++;
+        }
+
+        return ans;
+    }
+}
